d6a2119e6b2d8b6617bea35b312f5b54d72f6ba1
ahmar7 <ahmarjabbar7@gmail.com>
2022-06-02 20:32:19 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
 import React, { useState } from 'react';
 import './App.css'
 const App = () => {
-
   const [redActive, setredActive] = useState(false);
   const [yellowActive, setyellowActive] = useState(false);
   const [greenActive, setgreenActive] = useState(true);
-
+  const [btnState, setbtnState] = useState(false);
 
   let toggleClass = () => {
-    if (redActive == false) {
+    if (redActive === false) {
       setyellowActive(true);
     }
 
     setgreenActive(false);
     let Timer = setInterval(() => {
-      setredActive(
-        true
-      );
+      setbtnState(true);
       setyellowActive(false);
-
-    }, 2000
-
-    )
+      if (yellowActive === false) {
+        setredActive(true);
+      }
+    }, 2000);
 
     setTimeout(() => {
-      clearInterval(Timer)
-      setyellowActive(false)
-      setredActive(false)
-      setgreenActive(true)
+      setredActive(false);
+      setyellowActive(false);
+      setgreenActive(true);
+      setbtnState(false);
+      clearInterval(Timer);
     }, 4000);
-  }
+  };
 
   return (
     <div>
-      <div className='main-light'>
-        <div className={redActive ? "red-on" : 'red'}></div>
-        <div className={yellowActive ? "yellow-on" : 'yellow'}></div>
-        <div className={greenActive ? "green-on" : 'green'}></div>
+      <div className="main-light">
+        <div className={redActive ? "red-on" : "red"}></div>
+        <div className={yellowActive ? "yellow-on" : "yellow"}></div>
+        <div className={greenActive ? "green-on" : "green"}></div>
       </div>
-      <button onClick={toggleClass} className='traffic-btn'>Start</button>
+      <button disabled={btnState} onClick={toggleClass} className="traffic-btn">
+        Start
+      </button>
     </div>
   );
-}
+};
+
 
 export default App;
 
